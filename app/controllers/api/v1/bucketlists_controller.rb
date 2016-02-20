0ce7cefbf578c3f9d896c09aec767bf3656b4354
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-20 17:46:43 +0100

    Show All User Bucketlists
    https://www.pivotaltracker.com/story/show/114140403
    
    [finishes #114140403]

--- app/controllers/api/v1/bucketlists_controller.rb
+++ app/controllers/api/v1/bucketlists_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::BucketlistsController < ApplicationController
+   def index
+    bucketlists = Bucketlist.by_user(current_user)
+    render json: bucketlists, status: 200
+  end
+
   def create
     bucketlist = Bucketlist.new(bucket_params)
     bucketlist.user_id = current_user.id
@@ -7,11 +12,6 @@ class Api::V1::BucketlistsController < ApplicationController
     else
       render json: bucketlist.errors, status: 422
     end
-  end
-
-  def show
-    bucketlist = current_user.bucketlists.find_by_id(params[:id])
-    render json: bucketlist
   end
 
   def bucket_params
