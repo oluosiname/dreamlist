cf60692b71743ae96496d76df34f34cc96b2c80a
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-20 16:54:17 +0100

    Create new bucket list
    create a new bucketlist for a logged in user
    
    https://www.pivotaltracker.com/story/show/114140395
    
    [finishes #114140395]

--- app/controllers/api/v1/bucketlists_controller.rb
+++ app/controllers/api/v1/bucketlists_controller.rb
@@ -1,16 +1,11 @@
 class Api::V1::BucketlistsController < ApplicationController
-  def index
-    bucketlists = Bucketlist.by_user(current_user)
-    render json: bucketlists, status: 200
-  end
-
   def create
     bucketlist = Bucketlist.new(bucket_params)
-    bucketlist.user_id = current_user
+    bucketlist.user_id = current_user.id
     if bucketlist.save
       render json: bucketlist
     else
-      render json: bucketlist.errors
+      render json: bucketlist.errors, status: 422
     end
   end
 
--- app/poros/auth_token.rb
+++ app/poros/auth_token.rb
@@ -1,11 +1,11 @@
 require "jwt"
 class AuthToken
-  def self.encode(payload, exp=24.hours.from_now)
+  def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    payload = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
   end
 end
