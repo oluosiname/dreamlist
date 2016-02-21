70d222767f71cf0506d98df693f426f092872c58
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-21 14:33:18 +0100

    Update and delete an item
    Update or delete an item from a bucketlist
    
    https://www.pivotaltracker.com/story/show/114152539
    
    [finishes #114152539]

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,6 +10,22 @@ class Api::V1::ItemsController < ApplicationController
     end
   end
 
+  def update
+    if user_bucket(params[:bucketlist_id])
+      update_item(item_params, params[:bucketlist_id], params[:id])
+    else
+      render json: { message: " Bucketlistdoes not exist" }
+    end
+  end
+
+  def destroy
+    if user_bucket(params[:bucketlist_id])
+      delete_item(params[:bucketlist_id], params[:id])
+    else
+      render json: { message: "Bucketlist does not exist" }, status: 404
+    end
+  end
+
   def item_params
     params.permit(:name, :done)
   end
