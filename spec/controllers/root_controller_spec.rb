6764539dd70b454bc2e75fea99904635c9690a6a
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-24 14:44:57 +0100

    sets root for api

--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe RootController, type: :controller do
 
+  context "visit root" do
+    it "redirecst to apiary page" do
+      expect(get :index).to redirect_to ("http://docs.dreamlist.apiary.io/")
+    end
+  end
 end
