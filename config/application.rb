e093281a3b724c3aa32adf7515a7fb8b505bbc93
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-06-07 03:44:11 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,6 @@ Bundler.require(*Rails.groups)
 module DreamlistApi
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => %w{GET POST PUT DELETE OPTIONS}.join(","),
-    'Access-Control-Allow-Headers => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   }
   end
 end
