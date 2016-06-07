46dece86f977d6341eca958d76832f2a84769a3f
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-06-07 03:50:08 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,4 +25,12 @@ class ApplicationController < ActionController::API
   def user_token?
     User.find_by(token: get_token)
   end
+  
+  def set_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
+  end
+
+  end
 end
